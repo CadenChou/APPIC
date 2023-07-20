ee68c264b8436a5995cfe2b23102de1c73299ad8
Caden <cchou4092@gmail.com>
2023-07-19 21:17:50 -0700

    navbar change

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -14,7 +14,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 const pages = [
-    { title: 'Home Page', path: '/' },
+    { title: 'Cancer Types', path: '/body-diagram' },
     { title: 'About', path:'/about'},
     { title: 'Acknowledgements', path:'/acknowledgements'}
   ];
